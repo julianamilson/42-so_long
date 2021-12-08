e21ec2f892516747c391397d159449983b323c11
jmilson- <julianamilson@hotmail.com>
2021-12-08 19:24:30 +0100

    Update
    so_long.h: added more libraries
    so_long.c: comments are about the logics I should think about when
    developing the main

--- sources/so_long.c
+++ sources/so_long.c
@@ -1,22 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/04 18:01:47 by jmilson-          #+#    #+#             */
-/*   Updated: 2021/12/08 18:55:54 by jmilson-         ###   ########.fr       */
+/*   Created: 2021/12/08 19:05:51 by jmilson-          #+#    #+#             */
+/*   Updated: 2021/12/08 19:24:14 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <fcntl.h>
-# include "../libraries/minilibx/mlx.h"
+int	main(int argc, char **argv)
+{
+	
+	//argc eh o .so_long enquanto o outro argumento eh referente ao mapa
+	
+	//verificar se o mapa eh valido
 
-#endif
+	//se a verificacao der certo, iniciar tela e o jogo
+
+	// senao, aparecer erro e terminar execucao
+	
+}
